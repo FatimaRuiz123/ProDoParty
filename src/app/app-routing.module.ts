290d0af0ff053a6c4664bb5e860690bd3d8afaa0
FatimaRuiz123 <fatimiruiz141@gmail.com>
2022-10-17 17:47:43 -0500

    Creacion de componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetallesComponent } from './Component/detalles/detalles.component';
+import { HomeComponent } from './Component/home/home.component';
+import {CartComponent} from './Component/cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home', component: HomeComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path:'details',
+    component:DetallesComponent
+  },
+  {
+    path: 'cart',
+    component:CartComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Component/home/home.component';
+import { CartComponent } from './Component/cart/cart.component';
+import { DetallesComponent } from './Component/detalles/detalles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CartComponent,
+    DetallesComponent
   ],
   imports: [
     BrowserModule,
